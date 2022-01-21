f3c8719009af987ad89cc7914c6238c713d1aa67
lexllanes <allanes1016@gmail.com>
2022-01-21 12:54:39 -0800

    Saving New Files

---
+++ Recipe/script.js
@@ -0,0 +1,29 @@
+alert("Welcome to My Best Oatmeal Recipe!")
+
+
+
+//Function that starts a count down
+function startTimer(){
+  var counter = 60;
+
+  //setInterval method with the delay on the bottom
+  setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("count");
+      span.innerHTML = counter;
+    }
+    //When it reaches zero it will alert the user
+    if (counter === 0) {
+        alert('Oatmeal is done!');
+        clearInterval(counter); //clearInterval will stop the timer
+    }
+  }, 1000); //Delay interval = 1000ms which is one second
+}
+
+//This function grabs the elemend with the id="count" and changes it to green, then after calls the startTimer() function.
+function start()
+{
+    document.getElementById("count").style="color:green;";
+    startTimer();
+};
