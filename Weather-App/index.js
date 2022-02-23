d6c83b581a509cfbd4adb103dfca5932f18457d9
lexllanes <allanes1016@gmail.com>
2022-02-23 11:23:37 -0800

    Organizing

---
+++ Weather-App/index.js
@@ -0,0 +1,35 @@
+const http = require('http')
+var rp = require('request-promise');
+const port = 8080
+  
+const server = http.createServer(function (req, res) {
+      res.write('Test')
+      rp('api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=9c76e519b2c8bc3792e37dda0bf8ccab')
+    .then(function (htmlString) {
+        const response = htmlSptring;
+        // Process html...
+        console.log(htmlString.json())
+    })
+    .catch(function (err) {
+        // Crawling failed...
+    });
+      res.end()
+})
+  
+server.listen(port, function (error) {
+  
+    if (error) {
+        console.log('An error occurred', error);
+    }
+    else {
+        console.log('Server is listening on port' + port);
+    }
+})
+
+
+
+
+
+
+
+    
