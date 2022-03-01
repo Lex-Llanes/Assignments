94ff184450363d06b886684dd7439428f448cd84
lexllanes <allanes1016@gmail.com>
2022-03-01 14:54:37 -0800

    Final Commit

--- To-do/todo-cra/src/components/todo.js
+++ To-do/todo-cra/src/components/todo.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 
-const Todo = () => {
+const Todo = ({ todo, toggleTodo, index, deleteTodo}) => {
 
 
 
     return (
-        <>
-        
-        
-        </>
+        <div>
+            <label htmlFor={`todo-${index}`}>
+                <input
+                    id={`todo-${index}`}
+                    type="checkbox"
+                    checked={todo.isCompleted}
+                    onChange={() => toggleTodo(index)}
+                />
+            </label>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+        <div className="todo">{todo.text}</div>
+        <br></br>
+        </div>
     )
 }
 
